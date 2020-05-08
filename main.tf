de0298c3100a8b756f41cda36181ec6f1a8e3634
m0ssc0de <hi.paul.q@gmail.com>
2020-05-08 15:17:39 +0800

    just tested

--- main.tf
+++ main.tf
@@ -1,3 +1,9 @@
+provider "alicloud" {
+  access_key = "${var.access_key}"
+  secret_key = "${var.secret_key}"
+  region     = "${var.region}"
+}
+
 # Create a new ECS instance for a VPC
 resource "alicloud_security_group" "group" {
   name        = "tf_test_foo"
@@ -5,13 +11,31 @@ resource "alicloud_security_group" "group" {
   vpc_id      = "${alicloud_vpc.vpc.id}"
 }
 
+resource "alicloud_security_group_rule" "allow_all_tcp" {
+  type              = "ingress"
+  ip_protocol       = "tcp"
+  nic_type          = "intranet"
+  policy            = "accept"
+  port_range        = "22/22"
+  priority          = 1
+  security_group_id = "${alicloud_security_group.group.id}"
+  cidr_ip           = "0.0.0.0/0"
+}
+
+
 resource "alicloud_instance" "instance" {
   # cn-beijing
-  availability_zone = "cn-beijing-b"
+  availability_zone = "${var.zone}"
   security_groups   = "${alicloud_security_group.group.*.id}"
 
+  password = "Len0v0213"
+
   # series III
-  instance_type              = "ecs.n4.large"
+  instance_charge_type       = "PostPaid"
+  spot_strategy              = "SpotAsPriceGo"
+  auto_release_time          = "${timeadd(timestamp(), "1h")}"
+  internet_charge_type       = "PayByBandwidth"
+  instance_type              = "ecs.n1.tiny"
   system_disk_category       = "cloud_efficiency"
   image_id                   = "ubuntu_18_04_64_20G_alibase_20190624.vhd"
   instance_name              = "test_foo"
@@ -28,5 +52,5 @@ resource "alicloud_vswitch" "vswitch" {
   vpc_id = "${alicloud_vpc.vpc.id}"
   # Other parameters...
   cidr_block        = "172.16.0.0/21"
-  availability_zone = "cn-beijing-b"
+  availability_zone = "${var.zone}"
 }
---
+++ variables.tf
@@ -0,0 +1,16 @@
+variable "access_key" {
+  type = "string"
+}
+variable "secret_key" {
+  type = "string"
+}
+
+variable "region" {
+  type    = "string"
+  default = "cn-beijing"
+}
+
+variable "zone" {
+  type    = "string"
+  default = "cn-beijing-b"
+}
